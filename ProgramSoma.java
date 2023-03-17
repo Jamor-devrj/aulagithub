ddbfc2cedd5185b58e632421d3832c68cdabf60d
Jaime Moraes <jamor.devbr@gmail.comgit config --global user.email jamor.devbr@gmail.com>
2023-03-17 17:19:44 -0300

    criado ProgramSoma.java

---
+++ ProgramSoma.java
@@ -0,0 +1,30 @@
+package exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramSomaVetor {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Quantos numeros voce vai digitar? ");
+		int n = sc.nextInt();
+		
+		double[] vet = new double[n];
+		
+		double soma = 0.0;
+		
+		for (int i = 0; i < vet.length; i++) {
+			System.out.print("Digite um numero: ");
+			vet[i] = sc.nextDouble();
+			soma += vet[i];
+		}			
+		
+		System.out.printf("\nSOMA = %.2f%n", soma);
+					
+		sc.close();
+	}
+}
